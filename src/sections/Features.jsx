6ba62c670a7ae153fcc155d011c4ebadb3e3cafc
Tiago Gomes <tidgomes@gmail.com>
2025-01-20 15:02:15 -0300

    create a features constant with values to be displayed

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -1,5 +1,20 @@
+import { Element } from "react-scroll";
+import { features } from "../constants";
+
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <section>
+      <Element name="features">
+        <div className="container">
+          <div className="relative flex">
+            {features.map((feature) => (
+              <div key={feature.id}></div>
+            ))}
+          </div>
+        </div>
+      </Element>
+    </section>
+  );
 };
 
 export default Features;
